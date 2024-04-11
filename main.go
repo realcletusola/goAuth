31343bff8e1affa4e1f3dacdd7d8254a6188c012
cletushunsu <cletusola@outlook.com>
2024-04-11 13:33:44 -0400

    update main

--- main.go
+++ main.go
@@ -11,23 +11,22 @@ import (
 // main function 
 func main(){
 	// router instance 
-	r := routes.NewRouter()
+	r := router.NewRouter()
 
 	// define database connection string 
 	connectionString := "postgres://postgres:postgres@localhost/goauth?sslmode=disable"
 
 	// database instance 
-	conn, err := database.InitDb(connectionString)
-	if err != nil {
-		log.Panic(err)
-	}
+    err := database.InitDB(connectionString)
+    if err != nil {
+        log.Fatal("Error initializing database: ", err)
+    }
 
 	// make migrations
-	migration, err := database.createMigrations() 
-	if err != nil {
-		log.Println("Unable to make migrations")
-		log.Panic(err)
-	}
+	err = database.CreateMigrations()
+    if err != nil {
+        log.Fatal("Error running database migrations: ", err)
+    }
 
 	// start server 
 	log.Println("server listening on port 8080")
