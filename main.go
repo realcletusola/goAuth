e2c7e3449dbbe979e71fa9ae2a1be2c4a87abb7b
cletushunsu <cletusola@outlook.com>
2024-04-11 06:37:11 -0400

    update main

---
+++ main.go
@@ -0,0 +1,35 @@
+package main 
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/cletushunu/goAuth/Router"
+	"github.com/cletushunu/goAuth/Database"
+)
+
+// main function 
+func main(){
+	// router instance 
+	r := routes.NewRouter()
+
+	// define database connection string 
+	connectionString := 
+
+	// database instance 
+	conn, err := database.InitDb(connectionString)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	// make migrations
+	migration, err := database.createMigrations()
+	if err != nil {
+		log.Println("Unable to make migrations")
+		log.Panic(err)
+	}
+
+	// start server 
+	log.Println("server listening on port 8080")
+	http.ListenAndServe(":8080", r)	
+}
